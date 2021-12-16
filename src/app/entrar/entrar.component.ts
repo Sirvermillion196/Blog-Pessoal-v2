fce9cee1fe406d72e5a2c0de02f797dc82491901
unknown <guilherme196sousa@outlook.com>
2021-12-16 09:05:17 -0300

    Order by

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -14,9 +15,12 @@ export class EntrarComponent implements OnInit {
 
 userLogin: UserLogin = new UserLogin()
 
+
   constructor(
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
+
   ) { }
 
   ngOnInit() {
@@ -45,7 +49,7 @@ userLogin: UserLogin = new UserLogin()
       this.router.navigate(['/inicio'])
     }, erro =>{
       if(erro.status == 500){
-        alert('Usuário ou senha estão incorretos!')
+        this.alertas.showAlertDanger('Usuário ou senha estão incorretos!')
       }
     })
   }
